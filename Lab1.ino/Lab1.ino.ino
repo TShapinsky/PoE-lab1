7a57d80382ef3f37534a4cc2a5c5d09496dd8e33
Rowan Sharman <rsharman@olin.edu>
2017-09-05 10:29:39 -0400

    add back_and_forth, change switch statement

--- Lab1.ino/Lab1.ino.ino
+++ Lab1.ino/Lab1.ino.ino
@@ -1,18 +1,20 @@
+#include <LiquidCrystal.h>
+
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care
   of use the correct LED pin whatever is the board used.
   If you want to know what pin the on-board LED is connected to on your Arduino model, check
   the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
+
   This example code is in the public domain.
 
   modified 8 May 2014
   by Scott Fitzgerald
-  
+
   modified 2 Sep 2016
   by Arturo Guadalupi
 */
@@ -41,34 +43,64 @@ void setup() {
 void loop() {
   switch(MODE){
     case 0:
-      marquee_step();
+      off();
     break;
     case 1:
-      binary_step();
+      marquee_step();
     break;
     case 2:
+      back_and_forth();
+    break;
+    case 3:
+      binary_step();
+    break;
+    case 4:
+      compass();
+    break;
+    case 5:
     break;
   }
   animation_step++;
   delay(delay_length);
 }
 
+void off(){
+  for(int i = 0; i >  NUM_LEDS; i++){
+    digitalWrite(leds[i], LOW);
+  }
+}
+
 void marquee_step(){
   if(animation_step % (NUM_LEDS*2) < NUM_LEDS){
     digitalWrite(leds[animation_step%NUM_LEDS],HIGH);
-  }else{  
-    digitalWrite(leds[animation_step%NUM_LEDS],LOW); 
+  }else{
+    digitalWrite(leds[animation_step%NUM_LEDS],LOW);
   }
 }
 
 void binary_step(){
-  
+
 }
+
+void back_and_forth(){
+  if(animation_step % (NUM_LEDS*4) < NUM_LEDS*2){
+    if(animation_step % (NUM_LEDS*2) < NUM_LEDS){
+      digitalWrite(leds[animation_step%NUM_LEDS],HIGH);
+    }else{
+      digitalWrite(leds[animation_step%NUM_LEDS],LOW);
+    }
+  }
+  else{
+    if(animation_step % (NUM_LEDS*2) < NUM_LEDS){
+      digitalWrite(leds[3-(animation_step%NUM_LEDS)],HIGH);
+    }else{
+      digitalWrite(leds[3-(animation_step%NUM_LEDS)],LOW);
+    }
+  }
+}
+
 void change_mode(){
   if(millis() - last_mode_change > DEBOUNCE_TIME){
     MODE = (MODE + 1) % NUM_MODES;
   }
 }
-
-
-
